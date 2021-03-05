eeb9a1972946d8203e31c1298ecbd901c57b7398
camila <camilamarinslopes@id.uff.br>
2021-03-04 22:41:32 -0300

    Classe que cria softwares que podem ser instalados em contêineres

---
+++ Software.java
@@ -0,0 +1,7 @@
+package Apx3_2020_2;
+
+//AP3_2020_2_Q1
+public class Software { String software; int tamanho;
+
+    public Software(String soft, int tam) {
+        software = soft; tamanho = tam; } }
